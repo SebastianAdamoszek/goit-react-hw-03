ab2156d0d0dba315b0fde28f4f0c2038d96feb4d
Sebastian <sebaadamoszek@vp.pl>
2023-11-26 19:01:45 +0100

    corrected2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,20 @@
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
 import React, { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar.js';
 import ImageGallery from './ImageGallery/ImageGallery.js';
